053f34d45e783476b3ea52b6e995d1b65fa9cd87
Stefan Bruvik <stefan@colours.no>
2015-02-05 00:43:30 +0700

    Close #13 PR: Add sourceMappingURL to dest file.. Fixes #11

--- tasks/6to5.js
+++ tasks/6to5.js
@@ -1,5 +1,6 @@
 'use strict';
 var to5 = require('6to5');
+var path = require('path');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('6to5', 'Transpile ES6 to ES5', function () {
@@ -11,7 +12,12 @@ module.exports = function (grunt) {
 
 			var res = to5.transformFileSync(el.src[0], options);
 
-			grunt.file.write(el.dest, res.code);
+			var sourceMappingURL = '';
+			if (res.map) {
+				sourceMappingURL = '\n//# sourceMappingURL=' + path.basename(el.dest) + '.map';
+			}
+
+			grunt.file.write(el.dest, res.code + sourceMappingURL);
 
 			if (res.map) {
 				grunt.file.write(el.dest + '.map', JSON.stringify(res.map));
